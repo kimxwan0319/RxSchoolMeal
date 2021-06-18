517ce44008a4218eb96d690e68bbd594b65680e4
kimxwan0319 <kimxwan0319@naver.com>
2021-06-18 16:23:38 +0900

    ⚙️ :: (#2) RxAlamofire, RxSwift 설치

--- Package.swift
+++ Package.swift
@@ -12,15 +12,18 @@ let package = Package(
             targets: ["RxSchoolMeal"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.2.0"),
+        .package(url: "https://github.com/RxSwiftCommunity/RxAlamofire.git", from: "6.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RxSchoolMeal",
-            dependencies: []),
+            dependencies: [
+                "RxSwift",
+                "RxAlamofire"
+            ]),
         .testTarget(
             name: "RxSchoolMealTests",
             dependencies: ["RxSchoolMeal"]),
