c1f20bfb79f0f092ad56b1cc4793e44f8c5e3798
kimxwan0319 <kimxwan0319@naver.com>
2021-06-20 21:18:04 +0900

    ⚡️ :: StatusCode 설계

--- Sources/RxSchoolMeal/Network/StatusCode.swift
+++ Sources/RxSchoolMeal/Network/StatusCode.swift
@@ -1,1 +1,11 @@
+import Foundation
 
+enum StatusCode: Error {
+    case OK                  // 200; 성공
+    case badRequest          // 400; 올바르지 않은 요청
+    case unauthorized        // 401; 토큰 인증 실패
+    case forbidden           // 402; 권한 없음
+    case notFound            // 404; 찾을 수 없는 경로
+    case internalServerError // 500; 서버 에러
+    case unkown              // 알수 없는 에러
+}
