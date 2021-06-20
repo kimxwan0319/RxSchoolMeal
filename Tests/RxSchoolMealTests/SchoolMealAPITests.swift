08590e989ce5444f251d124eae55e6977320e036
kimxwan0319 <kimxwan0319@naver.com>
2021-06-21 05:54:44 +0900

    🧪 :: 오류 메세지를 더 명확하게 변경

--- Tests/RxSchoolMealTests/SchoolMealAPITests.swift
+++ Tests/RxSchoolMealTests/SchoolMealAPITests.swift
@@ -5,11 +5,11 @@ import RxSwift
 final class SchoolMealAPITests: XCTestCase {
 
     var disposeBag = DisposeBag()
-    
+
     override func tearDownWithError() throws {
         disposeBag = DisposeBag()
     }
-    
+
     func testGetSchoolInfo() throws {
         let expt = expectation(description: "Waiting done harkWork...")
         HTTPClient.shared.networking(.getSchoolInfo(schoolName: "대덕소프트웨어마이스터고"),
@@ -20,22 +20,21 @@ final class SchoolMealAPITests: XCTestCase {
             }, onFailure: { error in
                 switch error as? StatusCode {
                 case .badRequest:
-                    XCTFail("400 - badRequest")
+                    XCTFail("400 - badRequest Error")
                 case .unauthorized:
-                    XCTFail("401 - unauthorized")
+                    XCTFail("401 - unauthorized Error")
                 case .forbidden:
-                    XCTFail("402 - forbidden")
+                    XCTFail("402 - forbidden Error")
                 case .notFound:
-                    XCTFail("404 - notFound")
+                    XCTFail("404 - notFound Error")
                 case .internalServerError:
-                    XCTFail("500 - internalServerError")
+                    XCTFail("500 - internalServerError Error")
                 default:
                     XCTFail("Unkown Error")
                 expt.fulfill()
                 }
             })
             .disposed(by: disposeBag)
-        
         waitForExpectations(timeout: 5.0, handler: nil)
     }
     
