1af168788c4e762380ee19dd65d32312e8f1a8bc
kimxwan0319 <kimxwan0319@naver.com>
2021-06-21 05:56:29 +0900

    🧪 :: 급식 가져오는 API의 테스트 코드 작성

--- Tests/RxSchoolMealTests/SchoolMealAPITests.swift
+++ Tests/RxSchoolMealTests/SchoolMealAPITests.swift
@@ -37,6 +37,34 @@ final class SchoolMealAPITests: XCTestCase {
             .disposed(by: disposeBag)
         waitForExpectations(timeout: 5.0, handler: nil)
     }
-    
+
+    func testGetSchoolMeal() throws {
+        let expt = expectation(description: "Waiting done harkWork...")
+        HTTPClient.shared.networking(.getMeal(date: .today), MealModel.self)
+            .subscribe(onSuccess: { _ in
+                XCTAssertTrue(true)
+                expt.fulfill()
+            }, onFailure: { error in
+                switch error as? StatusCode {
+                case .badRequest:
+                    XCTFail("400 - badRequest Error")
+                case .unauthorized:
+                    XCTFail("401 - unauthorized Error")
+                case .forbidden:
+                    XCTFail("402 - forbidden Error")
+                case .notFound:
+                    XCTFail("404 - notFound Error")
+                case .internalServerError:
+                    XCTFail("500 - internalServerError Error")
+                default:
+                    XCTFail("Unkown Error")
+                expt.fulfill()
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+
 }
 
