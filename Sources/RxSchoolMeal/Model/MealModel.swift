8e8d6b79a009b86b42bd90431338e2a568ef48f8
kimxwan0319 <kimxwan0319@naver.com>
2021-06-20 23:34:09 +0900

    🐛 :: swap해서 코드를 싸버린거 고침

--- Sources/RxSchoolMeal/Model/MealModel.swift
+++ Sources/RxSchoolMeal/Model/MealModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct MealModel: Codable {
-    public let ATPT_OFCDC_SC_CODE: String
-    public let SD_SCHUL_CODE: String
-    public let SCHUL_NM: String
+    public let breakfast: [String]
+    public let lunch: [String]
+    public let dinner: [String]
 }
