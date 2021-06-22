648b501314be6465217efbddeb7114bdc768c683
kimxwan0319 <kimxwan0319@naver.com>
2021-06-22 23:16:57 +0900

    ♻️ :: 옵셔널일 필요가없는 프로퍼티 SCHUL_NM의 타입을 그냥 String으로 변경

--- Sources/RxSchoolMeal/Model/SchoolInfoModel.swift
+++ Sources/RxSchoolMeal/Model/SchoolInfoModel.swift
@@ -3,5 +3,5 @@ import Foundation
 public struct SchoolInfoModel: Codable {
     public let ATPT_OFCDC_SC_CODE: String
     public let SD_SCHUL_CODE: String
-    public let SCHUL_NM: String?
+    public let SCHUL_NM: String
 }
