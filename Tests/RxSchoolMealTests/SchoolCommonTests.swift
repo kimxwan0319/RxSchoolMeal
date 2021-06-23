13d099dd145db7a7d793a4902919022be1ee0768
kimxwan0319 <kimxwan0319@naver.com>
2021-06-23 17:06:24 +0900

    🧪 :: (#21) 학교 관련 테스트 코드 작성

--- Tests/RxSchoolMealTests/SchoolCommonTests.swift
+++ Tests/RxSchoolMealTests/SchoolCommonTests.swift
@@ -1,5 +1,54 @@
 import XCTest
+import RxSwift
 @testable import RxSchoolMeal
 
 final class SchoolCommonTests: XCTestCase {
+
+    var disposeBag = DisposeBag()
+    
+    override func tearDownWithError() throws {
+        disposeBag = DisposeBag()
+        SchoolCommon.shared = SchoolCommon()
+        let userDefault = UserDefaults.standard
+        userDefault.removeObject(forKey: "ATPT_OFCDC_SC_CODE")
+        userDefault.removeObject(forKey: "SD_SCHUL_CODE")
+        userDefault.removeObject(forKey: "SCHUL_NM")
+    }
+    
+    func testInitSchool() throws {
+        let expt = expectation(description: "Waiting done harkWork...")
+        let schoolName = "대덕소"
+        SchoolCommon.initSchool(schoolName: schoolName)
+        SchoolCommon.shared.isSet?.subscribe(onSuccess: {
+            XCTAssertTrue(SchoolCommon.shared.schoolName.contains(schoolName))
+            expt.fulfill()
+        }, onFailure: { err in
+            XCTFail(err.localizedDescription)
+            expt.fulfill()
+        })
+        .disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testInitSchoolWithSaved() {
+        let userDefaults = UserDefaults.standard
+        userDefaults.setValue("G10", forKey: "ATPT_OFCDC_SC_CODE")
+        userDefaults.setValue("7430310", forKey: "SD_SCHUL_CODE")
+        userDefaults.setValue("대덕소프트웨어마이스터고등학교", forKey: "SCHUL_NM")
+        let expt = expectation(description: "Waiting done harkWork...")
+        let schoolName = "대덕소"
+        SchoolCommon.initSchool(schoolName: schoolName)
+        SchoolCommon.shared.isSet?.subscribe(onSuccess: {
+            XCTAssertTrue(SchoolCommon.shared.schoolName.contains(schoolName))
+            expt.fulfill()
+        }, onFailure: { err in
+            XCTFail(err.localizedDescription)
+            expt.fulfill()
+        })
+        .disposed(by: disposeBag)
+        
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+
 }
