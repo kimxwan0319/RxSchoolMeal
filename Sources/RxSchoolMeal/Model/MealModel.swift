37f82097fbcbbf8e396914f27eece07b6805d2df
kimxwan0319 <kimxwan0319@naver.com>
2021-06-24 21:38:21 +0900

    ♻️ :: MealModel에 Quick Help 주석 추가

--- Sources/RxSchoolMeal/Model/MealModel.swift
+++ Sources/RxSchoolMeal/Model/MealModel.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 public struct MealModel: Codable {
+    /// 아침 🍽
     public let breakfast: [String]
+    /// 점심 🍽
     public let lunch: [String]
+    /// 저녁 🍽
     public let dinner: [String]
 }
