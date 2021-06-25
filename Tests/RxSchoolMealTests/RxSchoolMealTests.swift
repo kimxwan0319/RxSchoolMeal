cbb0fe7efb5385c174e430bf190e26f2a9c22d18
kimxwan0319 <kimxwan0319@naver.com>
2021-06-26 02:05:54 +0900

    🧪 :: (#21) Meal 클래스의 테스트 코드 작성

--- Tests/RxSchoolMealTests/RxSchoolMealTests.swift
+++ Tests/RxSchoolMealTests/RxSchoolMealTests.swift
@@ -1,5 +1,67 @@
-    import XCTest
-    @testable import RxSchoolMeal
+import XCTest
+import RxSwift
+@testable import RxSchoolMeal
 
-    final class RxSchoolMealTests: XCTestCase {
+final class RxSchoolMealTests: XCTestCase {
+
+    var disposeBag = DisposeBag()
+    
+    var date210625: Date {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyyMMdd"
+        return dateFormatter.date(from: "20210625")!
     }
+    var mealData210625 = MealModel(
+        breakfast: [
+            "간장계란밥",
+            "시금치된장국",
+            "피자소스함박",
+            "깍두기",
+            "비요뜨(초코링)"
+        ], lunch: [
+            "참치마요주먹밥",
+            "초계국수",
+            "찐만두",
+            "풋고추쌈장무침",
+            "열무김치",
+            "오렌지"
+        ],
+        dinner: []
+    )
+
+    override func setUpWithError() throws {
+        SchoolCommon.initSchool(schoolName: "대덕소")
+    }
+    
+    override func tearDownWithError() throws {
+        disposeBag = DisposeBag()
+        SchoolCommon.shared = SchoolCommon()
+    }
+    
+    func testGetMeal() throws {
+        let expt = expectation(description: "Waiting done harkWork...")
+        MEAL.getMeal(.anotherDate(date: date210625)).subscribe(onSuccess: { meal in
+            XCTAssertEqual(meal, self.mealData210625)
+            expt.fulfill()
+        }, onFailure: { err in
+            XCTFail(err.localizedDescription)
+            expt.fulfill()
+        })
+        .disposed(by: disposeBag)
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+    func testGetOneTimeMeal() throws {
+        let expt = expectation(description: "Waiting done harkWork...")
+        MEAL.getMeal(.anotherDate(date: date210625), timePart: .lunch).subscribe(onSuccess: { meal in
+            XCTAssertEqual(meal, self.mealData210625.lunch)
+            expt.fulfill()
+        }, onFailure: { err in
+            XCTFail(err.localizedDescription)
+            expt.fulfill()
+        })
+        .disposed(by: disposeBag)
+        waitForExpectations(timeout: 5.0, handler: nil)
+    }
+    
+}
