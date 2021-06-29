4e89ae94087d4f6a702b388a4fec7a68e55677ac
kimxwan0319 <kimxwan0319@naver.com>
2021-06-29 10:10:22 +0900

    ♻️ :: (#27) StatusCode extension에 localizedDescription관련 코드 작성

--- Sources/RxSchoolMeal/Network/StatusCode.swift
+++ Sources/RxSchoolMeal/Network/StatusCode.swift
@@ -12,3 +12,16 @@ internal enum StatusCode: Error {
     case unkown
 
 }
+
+extension StatusCode: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .badRequest:
+            return NSLocalizedString("학교이름이 잘못 되었습니다.", comment: "bad request")
+        case .internalServerError:
+            return NSLocalizedString("서버에 문제가 있네요.. 죄송합니다ㅠㅠ", comment: "internal server error")
+        case .unkown:
+            return NSLocalizedString("알 수 없는 에러가 발생하였습니다.", comment: "unkown")
+        }
+    }
+}
