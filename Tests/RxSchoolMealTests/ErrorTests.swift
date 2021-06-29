e56dc928f9e29da38364f1f589f9d162e1b48bf5
kimxwan0319 <kimxwan0319@naver.com>
2021-06-29 12:13:48 +0900

    🧪 :: (#27) StatusCode 에러의 localizedDescription 테스트 코드 작성

--- Tests/RxSchoolMealTests/ErrorTests.swift
+++ Tests/RxSchoolMealTests/ErrorTests.swift
@@ -2,5 +2,12 @@ import XCTest
 @testable import RxSchoolMeal
     
 final class ErrorTests: XCTestCase {
-    
+    func testStatusCodeLocalizedError() throws {
+        var statusCode = StatusCode.badRequest
+        XCTAssertEqual(statusCode.localizedDescription, "학교이름이 잘못 되었습니다.")
+        statusCode = StatusCode.internalServerError
+        XCTAssertEqual(statusCode.localizedDescription, "서버에 문제가 있네요.. 죄송합니다ㅠㅠ")
+        statusCode = StatusCode.unkown
+        XCTAssertEqual(statusCode.localizedDescription, "알 수 없는 에러가 발생하였습니다.")
+    }
 }
